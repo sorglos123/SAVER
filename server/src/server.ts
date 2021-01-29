fdaf65c3f8b479ea8f525b3c98134eae6a0bddc1
smartledore <b.krause94@web.de>
2021-01-29 13:45:17 +0100

    [B] Auslagerung von Variablen in .env

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,12 @@ import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import * as morgan from "morgan";
 
+require('dotenv').config({ 
+    path: __dirname + `/../.env` 
+});
+
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.BACKEND_PORT;
 
 app.use(morgan('combine'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,20 +20,23 @@ app.listen(port, () => {
 });
 
 /* Route 0: Hello World :-) */
-app.get('/status', (req,res) => {
+app.get('/', (req,res) => {
     res.send({
         message: 'Hello World!'
     });
 });
 
 /* Route 1: 
-1) Server bekommt Login-Daten... 
-    ... vom Frontend im Format { benutzername 'abc', passwort: 'xyz' }; 
-2) Danach schaut in der Datenbank, ob ein entsprechender Datensatz vorhanden ist.  
+1) Server bekommt Login-Daten vom Frontend im Format { benutzername 'abc', passwort: 'xyz' }; 
+2) Danach schaut in der Datenbank, ob ein entsprechender Datensatz vorhanden ist.
+3) Nach der Datenbank-Kommunikation kann über den res-Parameter eine 
+    Antwort ans Frontend geschickt werden.  
 */
 app.post('/register', (req, res) => {    
+    /* Konsolen-Ausgaben nur für Testzwecke */
     console.log('Benutzername: ' + req.body.email);
     console.log('Passwort: ' + req.body.passwd);
+    /* <- Hier kommt die Datenbank-Logik -> */
     res.send({
         message: `Registration of ${req.body.email} was successful!`
     })  
