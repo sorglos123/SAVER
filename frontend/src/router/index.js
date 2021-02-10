02be0fb5c9c67f58be1ff3a4fffa15db71f990b4
smartledore <b.krause94@web.de>
2021-02-10 15:15:40 +0100

    [B] Dashboard hinzugef.; Imports angepasst

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,42 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router';
-// import Register from '../views/Register.vue';
 import Login from '../views/Login.vue';
 
-const routes = [
-  {
-    path: '/',
-    name: 'Login',
-    component: Login
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-    children: [
-      {
-        path: '/helloworld',
-        component: () => import('../views/HelloWorld.vue')
-      },
-      {
-        // Eventuell path: '/user/:id'
-        path: '/user',
-        component: () => import('../views/User.vue')
-      },
-    ]
-  },
+const routes = [{
+        path: '/',
+        name: 'login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'register',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Register.vue')
+    },
+    {
+        path: '/home',
+        name: 'home',
+        component: () =>
+            import ('../views/home/Home.vue'),
+        children: [{
+                path: '/dashboard',
+                component: () =>
+                    import ('../views/home/Dashboard.vue')
+            },
+            {
+                // Eventuell path: '/user/:id'
+                path: '/user',
+                component: () =>
+                    import ('../views/home/User.vue')
+            },
+        ]
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
