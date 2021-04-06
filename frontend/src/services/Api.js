1089e98cca1a8a20d70d1dcb3c1c921d4253ca0e
sorglos123 <sascharichter@hotmail.com>
2021-04-06 11:15:07 +0200

    moving between repos

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -2,6 +2,6 @@ import * as axios from 'axios';
 
 export default () => {
     return axios.create({
-        baseURL: process.env.VUE_APP_BACKEND || 'http://192.168.178.77:4001/'
+        baseURL: process.env.VUE_APP_BACKEND || 'http://10.202.131.247:4001/'
     })
 }
